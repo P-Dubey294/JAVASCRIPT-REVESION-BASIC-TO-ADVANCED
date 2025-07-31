012a84f7b5b122fc7114dc3ae5a4cf75f0db9990
P-Dubey294 <dpratham649@gmail.com>
2025-07-31 20:41:14 +0530

    implemenation of defult parameter

---
+++ Array Methods/Defualt Parameter.js
@@ -0,0 +1,11 @@
+// giving a deafult value to be a parameter 
+ function sum (a , b=10){
+    return a + b
+ }
+ console.log(sum(5));
+
+
+  function add (a=1 , b){
+    return a + b
+ }
+ console.log(add(5));
