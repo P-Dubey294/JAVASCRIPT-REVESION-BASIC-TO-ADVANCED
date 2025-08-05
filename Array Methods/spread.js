b3ad5ea3985cf9971828955ecec39ca642b337eb
P-Dubey294 <dpratham649@gmail.com>
2025-08-05 20:50:19 +0530

    Spread in js

--- Array Methods/spread.js
+++ Array Methods/spread.js
@@ -15,3 +15,29 @@ const a = [2,3,4,5]
 const b = [1,6,7,8,9]
 const combine = [...a, ...b]
 console.log(combine);
+
+// Spread with object literals 
+let data = {
+    email : "dpratham649@gmail.com",
+    Password : "Pratha@3011"
+}
+const data_Copy = {...data, id:123};
+console.log(data_Copy);
+
+// Another example 
+let user = {
+    name : "PrathamkuarDubey",
+}
+let details = {
+    email : "dpratham649@gmail.com",
+    address : "Near Sai Raj Medical Civil Line Gondia"
+}
+const comb = {...user, ...details, id : 123}
+console.log(comb);
+
+//✅ 6. Using with Rest Parameters (Reverse of Spread)
+
+function rest(...args){
+    console.log(args);
+}
+rest(1,2,3,4,5,6,7)
