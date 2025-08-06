f8a4b81b9cd0183da65a63a3323e5963c2ab8317
P-Dubey294 <dpratham649@gmail.com>
2025-08-06 20:00:05 +0530

    Browser Enevrionment

---
+++ DOM/Browser Envrionment.js
@@ -0,0 +1,14 @@
+// Host Enviroment 
+// any plateform that run the javascript (eg browser , node js , smart devices)
+//Each host environment provides its own objects and APIs in addition
+// to JavaScript core features.
+
+
+// Window object 
+// Global Object 
+// global function and varibles are belogns to the window.
+
+function sayHi(){
+    console.log("hello");
+}
+window.sayHi(); // call gobla function via windows 
